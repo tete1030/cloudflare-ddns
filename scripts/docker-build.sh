57e3410d0c8c2cb5cd7dda4d910aeb46753f7ed0
Texot <tete1030@gmail.com>
2024-08-06 00:16:08 +0800

    custom docker image

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build --platform linux/amd64 --tag timothyjmiller/cloudflare-ddns:latest ../
+docker build --platform linux/amd64 --tag tete1030/cloudflare-ddns:latest ../
--- scripts/docker-run.sh
+++ scripts/docker-run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run timothyjmiller/cloudflare-ddns:latest
+docker run tete1030/cloudflare-ddns:latest
